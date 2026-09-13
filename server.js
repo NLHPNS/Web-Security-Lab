eca02d0181aa71ca74f1db841530fa828a109c5d
NLHPNS <nl1173193507@gmail.com>
2026-09-13 23:36:24 +1000

    add: authentication

--- server.js
+++ server.js
@@ -1,3 +1,34 @@
 import express from "express";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.static("."));
+
+const user = {
+    userName: "Jonas",
+    thePassword: "114514"
+};
+
+
+app.post("/login", (req, res) => {
+    console.log(req.body);
+
+    if (req.body.userName === user.userName &&
+        req.body.thePassword === user.thePassword
+    ) {
+        res.status(200).json({
+            message: "Login successful"
+        });
+    } else {
+        res.status(401).json({
+            message: "Login failed"
+        });
+    }
+
+    
+});
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
